77cd35a87390173039d2414d2d64bcd9eb81c58a
Mike Voynovich <mvoyno@gmail.com>
2021-03-17 18:32:01 -0400

    50% complete

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+router.post("/api/workouts", (req, res) => {
+  Workout.create({}).then(workoutData => res.json(workoutData)).catch(err => res.json(err)) 
+})
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}}, {new:true, runValidators:true}).then(workoutData => res.json(workoutData)).catch(err => res.json(err)) 
+})
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate
+})
+
+module.exports = router;
