15327b281a5f39ebfb6d79416503e05b61b46ebc
Mike Voynovich <mvoyno@gmail.com>
2021-03-18 13:19:33 -0400

    complete

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,18 @@ router.put("/api/workouts/:id", (req, res) => {
   Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}}, {new:true, runValidators:true}).then(workoutData => res.json(workoutData)).catch(err => res.json(err)) 
 })
 router.get("/api/workouts", (req, res) => {
-  Workout.aggregate
+  Workout.aggregate([
+    {$addFields:{
+      totalDuration: {$sum: '$exercises.duration'}
+    }}
+  ]).then(workoutData => res.json(workoutData)).catch(err => res.json(err))
+})
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {$addFields:{
+      totalDuration: {$sum: '$exercises.duration'}
+    }}
+  ]).sort({_id:-1}).limit(7).then(workoutData => res.json(workoutData)).catch(err => res.json(err))
 })
 
 module.exports = router;
